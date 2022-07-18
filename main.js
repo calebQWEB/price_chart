5a0d70fdcea3d35c9e33faa9f042d28a70e85a07
ochai caleb <calebochai@gmail.com>
2022-07-18 14:38:38 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,89 @@
+const theBars = document.querySelector(".bar-1")
+const thePrice = document.querySelector(".price-1")
+
+const theBars2 = document.querySelector(".bar-2")
+const thePrice2 = document.querySelector(".price-2")
+
+const theBars3 = document.querySelector(".bar-3")
+const thePrice3 = document.querySelector(".price-3")
+
+
+const theBars4 = document.querySelector(".bar-4")
+const thePrice4 = document.querySelector(".price-4")
+
+const theBars5 = document.querySelector(".bar-5")
+const thePrice5 = document.querySelector(".price-5")
+
+const theBars6 = document.querySelector(".bar-6")
+const thePrice6 = document.querySelector(".price-6")
+
+const theBars7 = document.querySelector(".bar-7")
+const thePrice7 = document.querySelector(".price-7")
+
+
+
+
+
+
+// MONDAY
+theBars.addEventListener("mouseover", function onmouseover() {
+    thePrice.style.display = "block"
+})
+
+theBars.addEventListener("mouseout", function onmouseout() {
+    thePrice.style.display = "none"
+})
+
+// TUESDAY
+theBars2.addEventListener("mouseover", function onmouseover() {
+    thePrice2.style.display = "block"
+}, 1000)
+
+theBars2.addEventListener("mouseout", function onmouseout() {
+    thePrice2.style.display = "none"
+}, 1000)
+
+// WEDNESDAY
+theBars3.addEventListener("mouseover", function onmouseover() {
+    thePrice3.style.display = "block"
+})
+
+theBars3.addEventListener("mouseout", function onmouseout() {
+    thePrice3.style.display = "none"
+})
+
+// THURSDAY
+theBars4.addEventListener("mouseover", function onmouseover() {
+    thePrice4.style.display = "block"
+})
+
+theBars4.addEventListener("mouseout", function onmouseout() {
+    thePrice4.style.display = "none"
+})
+
+// FRIDAY
+theBars5.addEventListener("mouseover", function onmouseover() {
+    thePrice5.style.display = "block"
+})
+
+theBars5.addEventListener("mouseout", function onmouseout() {
+    thePrice5.style.display = "none"
+})
+
+// SARTURDAY
+theBars6.addEventListener("mouseover", function onmouseover() {
+    thePrice6.style.display = "block"
+})
+
+theBars6.addEventListener("mouseout", function onmouseout() {
+    thePrice6.style.display = "none"
+})
+
+// SUNDAY
+theBars7.addEventListener("mouseover", function onmouseover() {
+    thePrice7.style.display = "block"
+})
+
+theBars7.addEventListener("mouseout", function onmouseout() {
+    thePrice7.style.display = "none"
+})
